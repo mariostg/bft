ed644cd2d2d76b03a21234961ef7d6a085c126fb
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-03 12:59:09 -0400

    Add basic fund list test.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_fund_list_and_retreive_it_later(self):
+        # Mario needs to create a new fund.  Let's visite the home page
+        self.browser.get("http://localhost:8000")
+
+        # Mario notices the page title and header mention Funds list
+        self.assertIn("Funds list", self.browser.title)
+        self.fail("Finish the test!")
+
+
+if __name__ == "__main__":
+    unittest.main()
