a8be2375a15c27959b234983389f8378049fcbdd
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-04 15:01:58 -0400

    Add StaticLiveServerTestCase to functional test and remove unittest

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,35 +1,31 @@
-from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class CostElementFundVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = WebDriver()
 
     def tearDown(self):
         self.browser.quit()
 
     def test_can_retreive_fund_form(self):
         # Need to add a new fund, visit the fund form page
-        self.browser.get("http://localhost:8000/fund/add")
+        self.browser.get(f"{self.live_server_url}/fund/add/")
 
         # Mario notices the page title and Form mention Create Fund
         self.assertIn("Fund Form", self.browser.title)
         form_header = self.browser.find_element(By.CLASS_NAME, "form__header").text
-        self.assertIn("Fund Form", form_header, "Fund form not found")
+        self.assertIn("Fund Form", form_header, "\nFund form not found")
 
     def test_can_retreive_fund_list(self):
         # Mario needs to create a new fund.  Let's visite the home page
-        self.browser.get("http://localhost:8000/fund/fund_page")
+        self.browser.get(f"{self.live_server_url}/fund_page/")
 
         # Mario notices the page title and header mention Funds list
         self.assertIn("Fund List", self.browser.title)
         table_id = self.browser.find_element(By.ID, "fund-table").text
         self.assertIn("Funds", table_id)
         # self.fail("Finish the test!")
-
-
-if __name__ == "__main__":
-    unittest.main()
