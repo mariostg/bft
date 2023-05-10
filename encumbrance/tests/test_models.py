5ab6123add30ce1999e08212f961aeb3a752f78f
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-10 09:32:52 -0400

    Add encumbrance model tests.

---
+++ encumbrance/tests/test_models.py
@@ -0,0 +1,94 @@
+from django.test import TestCase
+
+import unittest
+from encumbrance.models import Encumbrance, EncumbranceImport
+from lineitems.models import LineItems
+import os
+from main.settings import BASE_DIR
+
+
+class TestEncumbrance(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.path = os.path.join(BASE_DIR, "drmis_data")
+        print("PATH IS ", cls.path)
+        print(
+            "setUpTestData: Run once to set up non-modified data for all class methods."
+        )
+
+    def test_find_fund_center_line_good(self):
+        er = Encumbrance()
+        fc = er.find_fund("|Funds Center        |2184AA |")
+        self.assertEqual(fc, "2184AA")
+
+    def test_find_fundcenter_line_bad(self):
+        er = Encumbrance()
+        fc = er.find_fund("Funds Center        |2184AA |")
+        self.assertNotEqual(fc, "2184AA")
+
+    def test_find_fy_line_good(self):
+        er = Encumbrance()
+        fy = er.find_base_fy("|Base Fiscal Year    |2023   |")
+        self.assertEqual(fy, "2023")
+
+    def test_find_fy_line_bad(self):
+        er = Encumbrance()
+        fy = er.find_base_fy("Base Fiscal Year    |2023   |")
+        self.assertNotEqual(fy, "2023")
+
+    def test_find_layout_line_good(self):
+        er = Encumbrance()
+        fy = er.find_layout("|Layout              |/BFT_NP|")
+        self.assertEqual(fy, "/BFT_NP")
+
+    def test_find_layout_line_bad(self):
+        er = Encumbrance()
+        fy = er.find_layout("Layout              |/BFT_NP|")
+        self.assertNotEqual(fy, "/BFT_NP")
+
+    def test_is_dnd_cost_center_report(self):
+        fname = os.path.join(BASE_DIR, "drmis_data/encumbrance_P1a.txt")
+        print(f"File name is ::::{fname}")
+        er = Encumbrance(fname)
+        ok = er.is_dnd_cost_center_report()
+        self.assertTrue(ok)
+
+    def test_is_not_dnd_cost_center_report(self):
+        fname = os.path.join(self.path, "Errors_in_file.txt")
+        er = Encumbrance(fname)
+        ok = er.is_dnd_cost_center_report()
+        self.assertFalse(ok)
+
+    def test_clean_header(self):
+        header = "|Document N|Line Numbe|AcctAssNo.| Cur Year s|    Cur YR Bal|    Total Cur.|Funds Cent|Fund|Cost Cente|Order       |Document T|Encumbrance Type     |Line Text                                         |Prd.doc.no|Pred doc.i|Reference       |G/L Accoun|Due date  |Vendor nam                         |Created by  |"
+        header = header.split("|")
+        er = Encumbrance()
+        er.clean_header(header)
+        self.assertEqual(len(er.data["header"]), er.COLUMNS - 2)
+
+    def test_find_header_line(self):
+        fname = os.path.join(self.path, "encumbrance_P1a.txt")
+        er = Encumbrance(fname)
+        lineno = er.find_header_line()
+        self.assertGreater(lineno, 0)
+
+    def test_is_data_line(self):
+        er = Encumbrance()
+        lines = [
+            {"line": "|14518705  |       240|", "result": True},
+            {"line": "|1451870511|       240|", "result": True},
+            {"line": "|1451-8705 |       240|", "result": False},
+            {"line": "|Docum     |       240|", "result": False},
+            {"line": "Funds Center          ", "result": False},
+        ]
+        for line in lines:
+            self.assertEqual(er.is_data_line(line["line"]), line["result"])
+
+    # def test_insert_new_lines(self):
+    #     imported_lines = EncumbranceImport.objects.all()
+    #     li = LineItems()
+    #     li.insert_new(imported_lines)
+
+
+if __name__ == "__main__":
+    unittest.main()
