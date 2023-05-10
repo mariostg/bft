be6ea5aae0ad5fa941944b3317d6b812be2ea05f
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-10 11:09:47 -0400

    Create LineItem class.

--- lineitems/models.py
+++ lineitems/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from costcenter.models import CostCenter
+
+
+class LineItem(models.Model):
+    docno = models.CharField(max_length=10)
+    lineno = models.CharField(max_length=7)  # lineno : acctassno
+    spent = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    workingplan = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    fundcenter = models.CharField(max_length=6)
+    fund = models.CharField(max_length=4)
+    costcenter = models.ForeignKey(CostCenter, on_delete=models.RESTRICT)
+    internalorder = models.CharField(max_length=7, null=True, blank=True)
+    doctype = models.CharField(max_length=2, null=True, blank=True)
+    enctype = models.CharField(max_length=21)
+    linetext = models.CharField(max_length=50, null=True, blank=True, default="")
+    predecessordocno = models.CharField(
+        max_length=20, null=True, blank=True, default=""
+    )
+    predecessorlineno = models.CharField(
+        max_length=3, null=True, blank=True, default=""
+    )
+    reference = models.CharField(max_length=16, null=True, blank=True, default="")
+    gl = models.CharField(max_length=5)
+    duedate = models.DateField(null=True, blank=True)
+    vendor = models.CharField(max_length=50, null=True, blank=True)
+    createdby = models.CharField(max_length=50, null=True, blank=True, default="")
+    status = models.CharField(max_length=10, null=True, blank=True, default="")
+
+    def __str__(self):
+        text = f"{self.enctype} {self.docno}-{self.lineno}"
+        return str(text)
+
+    class Meta:
+        ordering = ["-docno", "lineno"]
+        verbose_name_plural = "Line Items"
