9f3732dba6f380dd7e1ce2c1f83209c17c21a58f
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-11 15:12:14 -0400

    Add argument to all Encumbrance() instantiation

--- encumbrance/tests/test_models.py
+++ encumbrance/tests/test_models.py
@@ -16,39 +16,37 @@ class TestEncumbrance(TestCase):
         self.assertTrue(os.path.exists(self.path), "Drmis data directory not found")
 
     def test_find_fund_center_line_good(self):
-        er = Encumbrance()
+        er = Encumbrance("fakefile")
         fc = er.find_fund("|Funds Center        |2184AA |")
         self.assertEqual(fc, "2184AA")
 
     def test_find_fundcenter_line_bad(self):
-        er = Encumbrance()
+        er = Encumbrance("fakefile")
         fc = er.find_fund("Funds Center        |2184AA |")
         self.assertNotEqual(fc, "2184AA")
 
     def test_find_fy_line_good(self):
-        er = Encumbrance()
+        er = Encumbrance("fakefile")
         fy = er.find_base_fy("|Base Fiscal Year    |2023   |")
         self.assertEqual(fy, "2023")
 
     def test_find_fy_line_bad(self):
-        er = Encumbrance()
+        er = Encumbrance("fakefile")
         fy = er.find_base_fy("Base Fiscal Year    |2023   |")
         self.assertNotEqual(fy, "2023")
 
     def test_find_layout_line_good(self):
-        er = Encumbrance()
+        er = Encumbrance("fakefile")
         fy = er.find_layout("|Layout              |/BFT_NP|")
         self.assertEqual(fy, "/BFT_NP")
 
     def test_find_layout_line_bad(self):
-        er = Encumbrance()
+        er = Encumbrance("fakefile")
         fy = er.find_layout("Layout              |/BFT_NP|")
         self.assertNotEqual(fy, "/BFT_NP")
 
     def test_is_dnd_cost_center_report(self):
-        fname = os.path.join(BASE_DIR, "drmis_data/encumbrance_P1a.txt")
-        print(f"File name is ::::{fname}")
-        er = Encumbrance(fname)
+        er = Encumbrance("encumbrance_P1a.txt")
         ok = er.is_dnd_cost_center_report()
         self.assertTrue(ok)
 
@@ -61,7 +59,7 @@ class TestEncumbrance(TestCase):
     def test_clean_header(self):
         header = "|Document N|Line Numbe|AcctAssNo.| Cur Year s|    Cur YR Bal|    Total Cur.|Funds Cent|Fund|Cost Cente|Order       |Document T|Encumbrance Type     |Line Text                                         |Prd.doc.no|Pred doc.i|Reference       |G/L Accoun|Due date  |Vendor nam                         |Created by  |"
         header = header.split("|")
-        er = Encumbrance()
+        er = Encumbrance("fakefile")
         er.clean_header(header)
         self.assertEqual(len(er.data["header"]), er.COLUMNS - 2)
 
@@ -76,7 +74,7 @@ class TestEncumbrance(TestCase):
         self.assertGreater(lineno, 0)
 
     def test_is_data_line(self):
-        er = Encumbrance()
+        er = Encumbrance("fakefile")
         lines = [
             {"line": "|14518705  |       240|", "result": True},
             {"line": "|1451870511|       240|", "result": True},
@@ -88,7 +86,7 @@ class TestEncumbrance(TestCase):
             self.assertEqual(er.is_data_line(line["line"]), line["result"])
 
     def test_line_to_csv_returns_list_or_none(self):
-        er = Encumbrance()
+        er = Encumbrance("fakefile")
         result_ok = er.line_to_csv(
             "|col1|col2|col3|col4|col5|col6|col7|col8|col9|col10|col11|col12|col13|col14|col15|col16|col17|col18|col19|col20|"
         )
