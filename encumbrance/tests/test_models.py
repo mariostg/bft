ce1f1514096e696eb881bad9c017585c176886fd
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-15 10:57:07 -0400

    Streamline encumbrance report file test names

--- encumbrance/tests/test_models.py
+++ encumbrance/tests/test_models.py
@@ -8,6 +8,11 @@ from main.settings import BASE_DIR
 
 
 class TestEncumbrance(TestCase):
+    GOODFILE = "encumbrance_small.txt"
+    WRONGFC = "encumbrance_wrong_fc.txt"
+    WRONGFY = "encumbrance_wrong_fy.txt"
+    WRONGLAYOUT = "encumbrance_wrong_layout.txt"
+
     @classmethod
     def setUpTestData(cls):
         cls.path = os.path.join(BASE_DIR, "drmis_data")
@@ -24,32 +29,32 @@ class TestEncumbrance(TestCase):
             Encumbrance("Fakefile")
 
     def test_find_fund_center_line_good(self):
-        er = Encumbrance("fakefile")
+        er = Encumbrance(self.GOODFILE)
         fc = er.find_fund("|Funds Center        |2184AA |")
         self.assertEqual(fc, "2184AA")
 
     def test_find_fundcenter_line_bad(self):
-        er = Encumbrance("fakefile")
+        er = Encumbrance(self.WRONGFC)
         fc = er.find_fund("Funds Center        |2184AA |")
         self.assertNotEqual(fc, "2184AA")
 
     def test_find_fy_line_good(self):
-        er = Encumbrance("fakefile")
+        er = Encumbrance(self.GOODFILE)
         fy = er.find_base_fy("|Base Fiscal Year    |2023   |")
         self.assertEqual(fy, "2023")
 
     def test_find_fy_line_bad(self):
-        er = Encumbrance("fakefile")
+        er = Encumbrance(self.WRONGFY)
         fy = er.find_base_fy("Base Fiscal Year    |2023   |")
         self.assertNotEqual(fy, "2023")
 
     def test_find_layout_line_good(self):
-        er = Encumbrance("fakefile")
+        er = Encumbrance(self.GOODFILE)
         fy = er.find_layout("|Layout              |/BFT_NP|")
         self.assertEqual(fy, "/BFT_NP")
 
     def test_find_layout_line_bad(self):
-        er = Encumbrance("fakefile")
+        er = Encumbrance(self.WRONGLAYOUT)
         fy = er.find_layout("Layout              |/BFT_NP|")
         self.assertNotEqual(fy, "/BFT_NP")
 
@@ -59,7 +64,7 @@ class TestEncumbrance(TestCase):
         self.assertTrue(ok)
 
     def test_is_not_dnd_cost_center_report(self):
-        fname = os.path.join(self.path, "drmis_data/Errors_in_file.txt")
+        fname = os.path.join(self.path, "encumbrance_errors_in_file.txt")
         er = Encumbrance(fname)
         ok = er.is_dnd_cost_center_report()
         self.assertFalse(ok)
@@ -67,7 +72,7 @@ class TestEncumbrance(TestCase):
     def test_clean_header(self):
         header = "|Document N|Line Numbe|AcctAssNo.| Cur Year s|    Cur YR Bal|    Total Cur.|Funds Cent|Fund|Cost Cente|Order       |Document T|Encumbrance Type     |Line Text                                         |Prd.doc.no|Pred doc.i|Reference       |G/L Accoun|Due date  |Vendor nam                         |Created by  |"
         header = header.split("|")
-        er = Encumbrance("fakefile")
+        er = Encumbrance(self.GOODFILE)
         er.clean_header(header)
         self.assertEqual(len(er.data["header"]), er.COLUMNS - 2)
 
@@ -77,12 +82,12 @@ class TestEncumbrance(TestCase):
         self.assertEqual(lineno, 0)
 
     def test_find_header_line_returns_non_zero_on_success(self):
-        er = Encumbrance("encumbrance_small.txt")
+        er = Encumbrance(self.GOODFILE)
         lineno = er.find_header_line()
         self.assertGreater(lineno, 0)
 
     def test_is_data_line(self):
-        er = Encumbrance("fakefile")
+        er = Encumbrance(self.GOODFILE)
         lines = [
             {"line": "|14518705  |       240|", "result": True},
             {"line": "|1451870511|       240|", "result": True},
@@ -94,7 +99,7 @@ class TestEncumbrance(TestCase):
             self.assertEqual(er.is_data_line(line["line"]), line["result"])
 
     def test_line_to_csv_returns_list_or_none(self):
-        er = Encumbrance("fakefile")
+        er = Encumbrance(self.GOODFILE)
         result_ok = er.line_to_csv(
             "|col1|col2|col3|col4|col5|col6|col7|col8|col9|col10|col11|col12|col13|col14|col15|col16|col17|col18|col19|col20|"
         )
@@ -108,7 +113,7 @@ class TestEncumbrance(TestCase):
         self.assertIsNone(result_bad, "Must returns none when list is not good")
 
     def test_run_all_stops_on_bad_cost_center_report(self):
-        er = Encumbrance("encumbrance_errors.txt")
+        er = Encumbrance(self.WRONGFC)
         self.assertFalse(er.run_all())
 
 
