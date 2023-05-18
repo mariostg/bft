a7b65e973db81755ca5ad8f03bcbfe9478cbd170
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-18 11:51:12 -0400

    Add cost centers and fund centers to admin panel

--- costcenter/admin.py
+++ costcenter/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from costcenter.models import Fund, CostCenter
+from costcenter.models import Fund, CostCenter, Source, FundCenter
 
 admin.site.register(Fund)
 admin.site.register(CostCenter)
+admin.site.register(Source)
+admin.site.register(FundCenter)
