1ff08567c8b0e38d8bc4f45b79ac0d47d4706d92
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-29 15:12:24 -0400

    Try to get some Management Command tests to work.  Not there yet

--- encumbrance/tests/test_commands.py
+++ encumbrance/tests/test_commands.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from encumbrance.management.commands import uploadcsv
+from io import StringIO
+from django.core.management import call_command
+from encumbrance.management.commands.tables import Command
 import unittest
 
 
@@ -16,3 +18,20 @@ class TestCostCenterParentAnalysis(TestCase):
 
     def test_is_true(self):
         self.assertTrue(True)
+
+
+# TODO This is not quite working
+class TestTablesQueryCommand(TestCase):
+    def call_command(self, *args, **kwargs):
+        call_command(
+            "tables",
+            *args,
+            stdout=StringIO(),
+            stderr=StringIO(),
+            **kwargs,
+        )
+
+    def test_dry_run(self):
+        Command.print_funds(self)
+        a = self.call_command()
+        print(a)
