cbc9b9ca300faa119624ba51c88068ad1d7d9edb
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-31 14:14:35 -0400

    Add list_display to fund, costcenter and fundcenter to admin panel

--- costcenter/admin.py
+++ costcenter/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 
 from costcenter.models import Fund, CostCenter, Source, FundCenter
 
-admin.site.register(Fund)
-admin.site.register(CostCenter)
+
+class CostCenterAdmin(admin.ModelAdmin):
+    list_display = ("costcenter", "parent", "fund", "source", "shortname", "isforecastable", "isupdatable")
+
+
+class FundCenterAdmin(admin.ModelAdmin):
+    list_display = ("fundcenter", "parent", "shortname")
+
+
+class FundAdmin(admin.ModelAdmin):
+    list_display = ("fund", "vote", "name")
+
+
+admin.site.register(Fund, FundAdmin)
+admin.site.register(CostCenter, CostCenterAdmin)
 admin.site.register(Source)
-admin.site.register(FundCenter)
+admin.site.register(FundCenter, FundCenterAdmin)
