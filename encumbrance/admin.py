86de1a9239833629fa50ea9ea99bfe45a2bb455d
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-31 13:26:19 -0400

    Add list_display to lineitems and encumbrance import admin panel

--- encumbrance/admin.py
+++ encumbrance/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from .models import EncumbranceImport
 
-admin.site.register(EncumbranceImport)
+
+class EncumbranceAdmin(admin.ModelAdmin):
+    list_display = (
+        "docno",
+        "lineno",
+        "fund",
+        "costcenter",
+        "doctype",
+        "enctype",
+        "spent",
+        "balance",
+        "workingplan",
+        "linetext",
+    )
+
+
+admin.site.register(EncumbranceImport, EncumbranceAdmin)
--- lineitems/admin.py
+++ lineitems/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from . import models
 
+
+class LineItemAdmin(admin.ModelAdmin):
+    list_display = [
+        "docno",
+        "lineno",
+        "doctype",
+        "enctype",
+        "fund",
+        "costcenter",
+        "status",
+        "spent",
+        "balance",
+        "workingplan",
+        "linetext",
+    ]
+
+
 # Register your models here.
-admin.site.register(models.LineItem)
+admin.site.register(models.LineItem, LineItemAdmin)
