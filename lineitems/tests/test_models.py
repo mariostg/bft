bc6ccfd48d26db7e09ad806de862e576f0fb915c
Mario St-Gelais <mario.stg@videotron.ca>
2023-05-31 11:17:53 -0400

    Assert LineItem orphan is a set

--- lineitems/tests/test_models.py
+++ lineitems/tests/test_models.py
@@ -59,6 +59,7 @@ class LineItemModelTest(TestCase):
         # check it out
         orphan = li.get_orphan_lines()
         self.assertIn(("999999", "123"), orphan)
+        self.assertIsInstance(orphan, set)
 
     def test_line_items_no_orphans(self):
         # bring lines in
@@ -68,3 +69,4 @@ class LineItemModelTest(TestCase):
         # check it out
         orphan = li.get_orphan_lines()
         self.assertEqual(0, len(orphan))
+        self.assertIsInstance(orphan, set)
