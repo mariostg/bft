5ae7ce79d75a7e233aa341d52c516e4dab6dc95c
Mario St-Gelais <mario.stg@videotron.ca>
2023-06-06 09:38:06 -0400

    feat:implement BFT home page

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("bft.urls")),
+    path("bft/", include("bft.urls")),
     path("fund/", include("costcenter.urls")),
     path("source/", include("costcenter.urls")),
     path("fundcenter/", include("costcenter.urls")),
