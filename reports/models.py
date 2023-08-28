0979fb54e25237bcee31d085dee37d8148cb50af
Mario St-Gelais <mario.stg@videotron.ca>
2023-08-28 11:08:16 -0400

    implement CostCenterMonthly model

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class CostCenterMonthly(models.Model):
+    fund = models.CharField(max_length=4)
+    source = models.CharField(max_length=24)
+    cost_center = models.CharField("Cost Center", max_length=6)
+
+    period = models.CharField(max_length=2)
+    fy = models.CharField(max_length=4)
+
+    spent = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    commitment = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    pre_commitment = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    fund_reservation = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    working_plan = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    charges = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    allocation = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    forecast = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        s = ""
+        field_names = [field.name for field in self._meta.fields]
+        for fn in field_names:
+            s += f"{fn} : {getattr(self,fn)}\n"
+        return s
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=(
+                    "fund",
+                    "source",
+                    "cost_center",
+                    "period",
+                    "fy",
+                ),
+                name="unique_cost_center_monthly_row",
+            )
+        ]
---
+++ reports/tests/test_CostCenterMonthlyModel.py
@@ -0,0 +1,57 @@
+import pytest
+from django.db.utils import IntegrityError
+from reports.models import CostCenterMonthly
+
+
+@pytest.mark.django_db
+class TestCostCenterMonthlyModel:
+    m1 = {
+        "fund": "C113",
+        "cost_center": "8486C1",
+        "source": "Army",
+        "fy": "2023",
+        "period": "1",
+        "spent": 3000,
+        "commitment": 1000,
+        "pre_commitment": 200,
+        "fund_reservation": 200,
+        "balance": 5000,
+        "working_plan": 4000,
+        "charges": -400,
+        "allocation": 1000,
+        "forecast": 6000,
+    }
+    duplicate = {
+        "fund": "C113",
+        "cost_center": "8486C1",
+        "source": "Army",
+        "fy": "2023",
+        "period": "1",
+        "spent": 3000,
+        "commitment": 1000,
+        "pre_commitment": 200,
+        "fund_reservation": 200,
+        "balance": 5000,
+        "working_plan": 4000,
+        "charges": -400,
+        "allocation": 1000,
+        "forecast": 6000,
+    }
+
+    @pytest.fixture
+    def setup(self):
+        # Will probably need a Command.handle() or something later to
+        # populate monthly data
+        pass
+
+    def test_insert_single_row(self):
+        md = CostCenterMonthly(**self.m1)
+        md.save()
+        assert 1 == CostCenterMonthly.objects.count()
+
+    def test_insert_duplicate_row(self):
+        md = CostCenterMonthly(**self.m1)
+        md.save()
+        md = CostCenterMonthly(**self.duplicate)
+        with pytest.raises(IntegrityError):
+            md.save()
