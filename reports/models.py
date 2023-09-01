c422af11c917943046ad897ac7b00bebdb190db0
Mario St-Gelais <mario.stg@videotron.ca>
2023-09-01 10:52:44 -0400

    set verbose name for CostCenterMonthly model to make things more convenient in dataframe setup

--- reports/models.py
+++ reports/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 
 class CostCenterMonthly(models.Model):
-    fund = models.CharField(max_length=4)
-    source = models.CharField(max_length=24)
+    fund = models.CharField("Fund", max_length=4)
+    source = models.CharField("Source", max_length=24)
     costcenter = models.CharField("Cost Center", max_length=6)
 
-    period = models.CharField(max_length=2)
-    fy = models.CharField(max_length=4)
+    period = models.CharField("Period", max_length=2)
+    fy = models.CharField("FY", max_length=4)
 
-    spent = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
-    commitment = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
-    pre_commitment = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
-    fund_reservation = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
-    working_plan = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
+    spent = models.DecimalField("Spent", max_digits=10, decimal_places=2, default=0, null=True)
+    commitment = models.DecimalField("Commitment", max_digits=10, decimal_places=2, default=0, null=True)
+    pre_commitment = models.DecimalField("Pre Commitment", max_digits=10, decimal_places=2, default=0, null=True)
+    fund_reservation = models.DecimalField("Fund Reservation", max_digits=10, decimal_places=2, default=0, null=True)
+    balance = models.DecimalField("Balance", max_digits=10, decimal_places=2, default=0, null=True)
+    working_plan = models.DecimalField("Working Plan", max_digits=10, decimal_places=2, default=0, null=True)
 
     def __str__(self):
         s = ""
