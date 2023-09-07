bb71977eb9506cf5d923b0e10c645cec19b15bd7
Mario St-Gelais <mario.stg@videotron.ca>
2023-09-07 12:28:32 -0400

    Fix Fund Center allocation tests

--- costcenter/tests/test_fundcenterallocation.py
+++ costcenter/tests/test_fundcenterallocation.py
@@ -1,5 +1,12 @@
 import pytest
-from costcenter.models import CostCenterAllocation, FundCenterManager, FundCenterAllocation, Fund, CostCenterManager
+from costcenter.models import (
+    CostCenterAllocation,
+    FundCenterManager,
+    FundCenterAllocation,
+    Fund,
+    CostCenterManager,
+    FundManager,
+)
 from bft.management.commands import populate, uploadcsv
 from django.db.models import Sum
 
@@ -21,9 +28,12 @@ class TestFundCenterAllocation:
         assert a.fundcenter.fundcenter == "1111AB"
 
     def test_get_costcenter_subordinate_allocation(self, setup):
+        FundCenterAllocation.objects.all().delete()
+        CostCenterAllocation.objects.all().delete()
         fc = FundCenterManager().fundcenter("1111AB")
+        fund = FundManager().fund("C113")
         fc_alloc = FundCenterAllocation.objects.create(fundcenter=fc, amount=1000, fy=2023, quarter="1", fund=self.C113)
-        assert 1 == fc_alloc.fundcenter.fundcenter
+        assert "1111AB" == fc_alloc.fundcenter.fundcenter
 
         cc = CostCenterManager().cost_center(costcenter="8486C1")
         CostCenterAllocation.objects.create(costcenter=cc, amount=1000, fy=2023, quarter="1", fund=self.C113)
@@ -31,19 +41,20 @@ class TestFundCenterAllocation:
         cc = CostCenterManager().cost_center(costcenter="8486C2")
         CostCenterAllocation.objects.create(costcenter=cc, amount=2000, fy=2023, quarter="1", fund=self.C113)
 
-        fc_alloc = FundCenterAllocation
+        fc_alloc = FundCenterAllocation(fundcenter=fc, fund=fund, fy=2023, quarter=1)
         sub_alloc = CostCenterManager().get_sub_alloc(fc_alloc)
-        print(sub_alloc)
+        assert 2 == sub_alloc.count()
 
     def test_get_fundcenter_subordinate_allocation(self, setup):
-        fc = FundCenterManager().fundcenter("1111AB")
+        FundCenterAllocation.objects.all().delete()
+        fc = FundCenterManager().fundcenter("1111AA")
         fc_alloc = FundCenterAllocation.objects.create(fundcenter=fc, amount=1000, fy=2023, quarter="1", fund=self.C113)
-        assert 1 == fc_alloc.id
+        assert "1111AA" == fc_alloc.fundcenter.fundcenter
 
-        cc = FundCenterManager().fundcenter(fundcenter="2222BA")
+        cc = FundCenterManager().fundcenter(fundcenter="1111AB")
         FundCenterAllocation.objects.create(fundcenter=cc, amount=1000, fy=2023, quarter="1", fund=self.C113)
 
-        cc = FundCenterManager().fundcenter(fundcenter="2222BB")
+        cc = FundCenterManager().fundcenter(fundcenter="1111AC")
         FundCenterAllocation.objects.create(fundcenter=cc, amount=2000, fy=2023, quarter="1", fund=self.C113)
 
         sub_alloc = FundCenterManager().get_sub_alloc(fc_alloc)
