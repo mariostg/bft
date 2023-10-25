845476704d2e649abca4fee1f3f28e4c9abda6e0
Mario St-Gelais <mario.stg@videotron.ca>
2023-10-25 12:29:59 -0400

    Fine tune behaviour of costcentercharges command to handle fy and perio

--- bft/management/commands/costcentercharges.py
+++ bft/management/commands/costcentercharges.py
@@ -5,6 +5,9 @@ from charges.models import CostCenterChargeProcessor
 class Command(BaseCommand):
     """
     Import DRMIS Cost Center Charges report into CostCenterChargeImport table and subsequently into Cost Center Monthly Data table.  drmis_datan folder must exist and contains reports as defined in test files for development purposes.
+
+    typical usage:
+        python manage.py costcentercharges --to_csv drmis_data/charges_cc_test.txt --fy 2023 --period 7 --to_table
     """
 
     help = "Import Cost Center Charges into the system."
@@ -20,6 +23,18 @@ class Command(BaseCommand):
             action="store_true",
             help="Import DRMIS Report into import table",
         )
+        parser.add_argument(
+            "--fy",
+            type=str,
+            required=True,
+            help="Fiscal year to apply to the charges",
+        )
+        parser.add_argument(
+            "--period",
+            type=str,
+            required=True,
+            help="Period expected to see in charges",
+        )
 
         parser.add_argument(
             "cc_charge_file",
@@ -27,13 +42,14 @@ class Command(BaseCommand):
             help="cc charge drmis report full path",
         )
 
-    def handle(self, *args, to_csv, to_table, **options):
+    def handle(self, *args, to_csv, to_table, fy, period, **options):
         cp = CostCenterChargeProcessor()
         csv_processed = False
         if to_csv:
             rawtextfile = options["cc_charge_file"]
-            print(f"Send to csv using {rawtextfile}")
+            print(f"Send to csv using {rawtextfile} for {fy}, period {period}")
+            print(args)
             cp.to_csv(rawtextfile)
             csv_processed = True
         if to_table and csv_processed:
-            cp.csv2cost_center_charge_import_table()
+            cp.csv2cost_center_charge_import_table(fy, period)
