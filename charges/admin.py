43fc0ffb582292c9f59ecc5823236fe2bbc4cf3d
Mario St-Gelais <mario.stg@videotron.ca>
2023-10-26 10:09:26 -0400

    Add CostCenterChargeMonthly to admin view

--- charges/admin.py
+++ charges/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import CostCenterChargeImport
+from .models import CostCenterChargeMonthly, CostCenterChargeImport
 
 
 class CostCenterChargeImportAdmin(admin.ModelAdmin):
     list_display = ["fund", "costcenter", "ref_doc_no", "doc_type", "amount", "posting_date", "period", "fy"]
 
 
+class CostCenterChargeMonthlyAdmin(admin.ModelAdmin):
+    list_display = ["fund", "costcenter", "amount", "fy", "period"]
+
+
 admin.site.register(CostCenterChargeImport, CostCenterChargeImportAdmin)
+admin.site.register(CostCenterChargeMonthly, CostCenterChargeMonthlyAdmin)
