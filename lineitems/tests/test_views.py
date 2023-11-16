62a33558c9ce337f430b28af962316efd8ec6977
Mario St-Gelais <mario.stg@videotron.ca>
2023-11-16 13:19:09 -0500

    Delete tests no longer relevant

--- lineitems/tests/test_views.py
+++ lineitems/tests/test_views.py
@@ -19,19 +19,3 @@ class CostCenterLineItemTest(TestCase):
         c = Client()
         response = c.get(f"/lineitem/lineitem/?costcenter={{cc_id}}")
         self.assertEqual(200, response.status_code)
-
-    def test_url_is_bad(self):
-        c = Client()
-        response = c.get("/lineitem/lineitem/?costcenter=99")
-        # self.assertEqual(404, response.status_code)
-        assert True == ("There appears to be no line items in 1111" in str(response.content))
-
-    def test_view_uses_correct_template(self):
-        response = self.client.get("/lineitem/costcenter/8484WA/")
-        self.assertTemplateUsed(response, "lineitems/lineitem-table.html")
-
-    def test_cost_center_line_items_has_lines(self):
-        c = Client()
-        response = c.get("/lineitem/lineitem/?costcenter=8484WA")
-        self.assertEqual(200, response.status_code)
-        self.assertGreater(len(response.context["data"]), 0)
