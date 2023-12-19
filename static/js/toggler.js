bceaddc9850e642fae39d697dab3751fcf73a23a
Mario St-Gelais <mario.stg@videotron.ca>
2023-12-19 13:51:41 -0500

    Display line item comments with toggling option

---
+++ static/js/toggler.js
@@ -0,0 +1,30 @@
+function toggle_comments() {
+    const toggling = document.querySelectorAll(".cantoggle");
+    toggling.forEach((item) => {
+        item.classList.toggle("hide");
+    });
+}
+
+function set_comment_visibility(hidden) {
+    const toggling = document.querySelectorAll(".cantoggle");
+    toggling.forEach((item) => {
+        hidden ? item.classList.add("hide") : item.classList.remove("hide");
+    });
+}
+
+const text_toggler = document.getElementById("comment-toggler");
+if (text_toggler) {
+    console.log("Hidden " + JSON.parse(sessionStorage.getItem("comment_hidden")));
+    text_toggler.addEventListener("click", (event) => {
+        let comment_hidden = !JSON.parse(sessionStorage.getItem("comment_hidden"));
+        sessionStorage.setItem("comment_hidden", comment_hidden);
+        console.log(comment_hidden);
+        toggle_comments();
+    });
+}
+addEventListener("load", (event) => {
+    comment_hidden = JSON.parse(sessionStorage.getItem("comment_hidden"));
+    if (comment_hidden) {
+        set_comment_visibility(true);
+    }
+});
