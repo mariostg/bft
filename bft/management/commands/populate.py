03d778cdb209dfe88826e8ba18d23a231b00c293
Mario St-Gelais <mario.stg@videotron.ca>
2024-01-24 12:34:02 -0500

    Add documentation to populate.py

--- bft/management/commands/populate.py
+++ bft/management/commands/populate.py
@@ -1,3 +1,15 @@
+"""
+An administration tool for development purposes.  It serves to fill in some funds, sources, fund centers and cost centers.  Values fed in the database should match what is expected from the data to be used when running uploadtocsv which also uses test encumbrance report data.
+
+Typical usage:
+    python manage.py populate
+
+Populate command uses the data available from the test-data folder.  This folder contains all the CSV files necessary to upload basic financial structure and allocations.
+
+!!! warning
+    populate command is only available in DEBUG mode.  It will overwrite all existing data in the system.
+
+"""
 import os
 from django.core.management.base import BaseCommand, CommandError
 from main.settings import BASE_DIR, DEBUG
@@ -23,10 +35,6 @@ from bft import finstructure, uploadprocessor
 
 
 class Command(BaseCommand):
-    """
-    A class to be used only for development purposes.  It serves to fill in some funds, sources, fund centers and cost centers.  Values fed in the database should match what is expected from the data to be used when running uploadtocsv which also uses test encumbrance report data.
-    """
-
     def handle(self, *args, **options):
         if DEBUG:
             LineForecast.objects.all().delete()
@@ -52,18 +60,36 @@ class Command(BaseCommand):
             print("This capability is only available when DEBUG is True")
 
     def set_fund(self):
+        """
+        Populate the database with all funds contained in the csv file.
+        This operation is required before uploading cost centers.
+        """
         uploadprocessor.FundProcessor("test-data/funds.csv", None).main()
 
     def set_source(self):
+        """
+        Populate the database with all sources contained in the csv file.
+        This operation is required before uploading cost centers.
+        """
         uploadprocessor.SourceProcessor("test-data/source.csv", None).main()
 
     def set_fund_center(self):
+        """Populate the database with all fund centers contained in the csv file.
+        This operation is required before uploading cost centers.
+        """
         uploadprocessor.FundCenterProcessor("test-data/fundcenters.csv", None).main()
 
     def set_cost_center(self):
+        """Populate the database with all fund centers contained in the csv file.
+        All funds, sources and fund centers the the coste centers relies on must be contained in the
+        related csv file.
+        """
         uploadprocessor.CostCenterProcessor("test-data/costcenters.csv", None).main()
 
     def set_bft_status(self):
+        """
+        Set the current fiscal year to 2023, quarter to 1 and period to 1.
+        """
         bs = BftStatus()
         bs.status = "FY"
         bs.value = 2023
@@ -81,11 +107,19 @@ class Command(BaseCommand):
         print(f"Set Period to {BftStatusManager().period()}")
 
     def set_fund_center_allocation(self):
+        """Populate the database with all fund center allocations containted in the csv file.
+        File must have FY of 2023 and quarter to 1.  All fund centers and funds indicated in
+        the file must exists in the database.
+        """
         uploadprocessor.FundCenterAllocationProcessor(
             "test-data/fundcenter-allocations.csv", 2023, "1", None
         ).main()
 
     def set_cost_center_allocation(self):
+        """Populate the database with all cost center allocations containted in the csv file.
+        File must have FY of 2023 and quarter to 1.  All cost centers and funds indicated in
+        the file must exists in the database.
+        """
         uploadprocessor.CostCenterAllocationProcessor(
             "test-data/costcenter-allocations.csv", 2023, "1", None
         ).main()
