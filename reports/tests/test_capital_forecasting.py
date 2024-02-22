17761422e2e57ab909d0a0de50e5aab668e664c9
Mario St-Gelais <mario.stg@videotron.ca>
2024-02-22 10:47:19 -0500

    Capital forecasting tests added.

--- reports/tests/test_capital_forecasting.py
+++ reports/tests/test_capital_forecasting.py
@@ -23,7 +23,7 @@ class TestCapitalForecasting:
         assert "C113" == self.cf.fund.fund
         assert "2184DA" == self.cf.fundcenter.fundcenter
         assert "C.999999" == self.cf.capital_project.project_no
-        assert 2024 == self.cf.fy
+        assert [2024] == self.cf.fy
 
     def test_setparams_with_fy_range(self, populate):
         self.cf = capitalforecasting.CapitalReport("2184da", "c113", "C.999999", [2020, 2023])
@@ -40,12 +40,51 @@ class TestCapitalForecasting:
         assert 1 == self.cf.quarter
         assert [2020, 2023] == self.cf.fy
 
+    def test_dataset_single_fy(self, populate):
+        self.cf = capitalforecasting.CapitalReport("2184da", "c113", "C.999999", 2024)
+        self.cf.dataset()
+        assert 1 == self.cf._dataset.count()
+
+    def test_dataset_multiple_fy(self, populate):
+        self.cf = capitalforecasting.CapitalReport("2184da", "c113", "C.999999", [2022, 2023, 2024])
+        self.cf.dataset()
+        assert 3 == self.cf._dataset.count()
+
 
 @pytest.mark.django_db
 class TestCapitalForecastingHistoricalOutlookReport:
     @pytest.fixture
-    def setup(self):
-        self.cf = capitalforecasting.HistoricalOutlookReport("2184da", "c113", "c.999999", 2021)
+    def populate(self):
+        hnd = populate.Command()
+        hnd.handle()
 
-    def test_dataset(self, setup):
-        print(self.cf.hor_dataset)
+    def test_dataset(self, populate):
+        cf = capitalforecasting.HistoricalOutlookReport("2184da", "c113", "c.999999", 2021)
+        print(cf._dataset)
+
+
+@pytest.mark.django_db
+class TestCapitalForecastingFEARStatus:
+    @pytest.fixture
+    def populate(self):
+        hnd = populate.Command()
+        hnd.handle()
+
+    def test_dataset(self, populate):
+        cf = capitalforecasting.FEARStatusReport(
+            "2184da",
+            "c113",
+            2021,
+            "c.999999",
+        )
+        assert 0 == len(cf._dataset)
+
+    def test_dataframe(self, populate):
+        cf = capitalforecasting.FEARStatusReport(
+            "2184da",
+            "c113",
+            2021,
+            "c.999999",
+        )
+        cf.dataframe()
+        print(cf.df)
