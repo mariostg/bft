8a1f50fa125324fb3cbd853a441d3766b658662c
Mario St-Gelais <mario.stg@videotron.ca>
2024-04-22 14:03:22 -0400

    install debug-toolbar

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     # path("charges/", include("charges.urls")),
     # path("users/", include("users.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 
