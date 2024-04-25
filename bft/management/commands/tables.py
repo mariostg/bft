beb355a7cf19f95a8486046e82a7f4e6da4c443f
Mario St-Gelais <mario.stg@videotron.ca>
2024-04-25 13:46:53 -0400

    implement django-rich and setup for costcenters and funds commands

--- bft/management/commands/tables.py
+++ bft/management/commands/tables.py
@@ -1,9 +1,10 @@
-from django.core.management.base import BaseCommand
+from django_rich.management import RichCommand
+from rich.table import Table
 
 from bft.models import CostCenter, Fund, Source
 
 
-class Command(BaseCommand):
+class Command(RichCommand):
     def add_arguments(self, parser):
         # Positional arguments
         # parser.add_argument("funds", nargs="+", type=str)
@@ -25,13 +26,37 @@ class Command(BaseCommand):
 
     def print_funds(self):
         obj = Fund.objects.all()
+        table = Table(title="Recorded Funds")
+        table.add_column("Fund", no_wrap=True)
+        table.add_column("Description", no_wrap=True)
         for elem in obj:
-            print(f"{elem.fund}\t{elem.name}")
+            table.add_row(
+                elem.fund,
+                elem.name,
+            )
+        self.console.print(table)
 
     def print_costcenters(self):
         obj = CostCenter.objects.all()
+        table = Table(title="Recorded Cost Centers")
+        table.add_column("Cost Center", no_wrap=True)
+        table.add_column("Description", no_wrap=True)
+        table.add_column("Fund", no_wrap=True)
+        table.add_column("Source", no_wrap=True)
+        table.add_column("Parent", no_wrap=True)
+        table.add_column("Forecastable", no_wrap=True)
+        table.add_column("Updatable", no_wrap=True)
         for elem in obj:
-            print(f"{elem.costcenter}\t{elem.shortname}")
+            table.add_row(
+                elem.costcenter,
+                elem.shortname,
+                elem.fund.fund,
+                elem.source.source,
+                elem.costcenter_parent.fundcenter,
+                str(elem.isforecastable),
+                str(elem.isupdatable),
+            )
+        self.console.print(table)
 
     def print_sources(self):
         obj = Source.objects.all()
