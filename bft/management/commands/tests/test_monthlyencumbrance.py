af0eed0d9bd918592d7ce95de71dd60201d81abc
Mario St-Gelais <mario.stg@videotron.ca>
2024-05-02 15:27:04 -0400

    rename monthlydata related files and classes/methods

--- bft/management/commands/tests/test_monthlyencumbrance.py
+++ bft/management/commands/tests/test_monthlyencumbrance.py
@@ -9,7 +9,7 @@ from reports.models import CostCenterMonthlyEncumbrance
 
 
 @pytest.mark.django_db
-class TestCommandMonthlyData:
+class TestCommandMonthlyEncumbrance:
 
     def call_command(self, command, *args, **kwargs):
         out = StringIO()
@@ -23,12 +23,12 @@ class TestCommandMonthlyData:
         return out.getvalue()
 
     def test_bftstatus(self):
-        out = self.call_command("monthlydata", "--bftstatus")
+        out = self.call_command("monthlyencumbrance", "--bftstatus")
         assert True == ("Current fiscal year" in out)
         assert True == ("Current quarter" in out)
         assert True == ("Current period" in out)
 
-    def test_running_monthlydata_has_lines(self):
+    def test_running_monthlyencumbrance_has_lines(self):
         """Check that executing monthly data for given fy, period, contains the cost center and number of lines expected."""
         self.call_command("populate")
         self.call_command("uploadcsv", "test-data/encumbrance_2184A3.txt")
@@ -37,10 +37,10 @@ class TestCommandMonthlyData:
         assert ccmgr.exists("8484WA") == True
         assert 7 == LineItem.objects.count()
 
-        self.call_command("monthlydata", "--update", "--fy", "2023", "--period", "1")
+        self.call_command("monthlyencumbrance", "--update", "--fy", "2023", "--period", "1")
         assert 2 == CostCenterMonthlyEncumbrance.objects.count()
 
-    def test_monthlydata_view_invalid_period(self):
+    def test_monthlyencumbrance_view_invalid_period(self):
         self.call_command("populate")
         self.call_command("uploadcsv", "test-data/encumbrance_2184A3.txt")
 
@@ -53,9 +53,9 @@ class TestCommandMonthlyData:
                 "Period [19] not valid.  Must be one of ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14')"
             ),
         ):
-            self.call_command("monthlydata", "--view", "--period", "19")
+            self.call_command("monthlyencumbrance", "--view", "--period", "19")
 
-    def test_monthlydata_view_missing_costcenter(self):
+    def test_monthlyencumbrance_view_missing_costcenter(self):
         self.call_command("populate")
         self.call_command("uploadcsv", "test-data/encumbrance_2184A3.txt")
 
@@ -63,4 +63,4 @@ class TestCommandMonthlyData:
         assert ccmgr.exists("8484WA") == True
         assert 7 == LineItem.objects.count()
 
-        self.call_command("monthlydata", "--view", "--period", "1")
+        self.call_command("monthlyencumbrance", "--view", "--period", "1")
