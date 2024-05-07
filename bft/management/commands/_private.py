92c4f6c290dd324f2ac383604ddff1094c372403
Mario St-Gelais <mario.stg@videotron.ca>
2024-05-07 13:44:58 -0400

    Create and implement management command private UserInput class

---
+++ bft/management/commands/_private.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+from bft.models import BftStatus, CostCenter, CostCenterManager
+from bft.conf import PERIODKEYS
+
+
+class UserInput(BaseCommand):
+
+    def _get_user_input(self, hint: str) -> bool:
+        _continue = False
+        choice = input(f"Continue using {hint} ? [y/n] ")
+        if not choice or choice in ("N", "n"):
+            _continue = False
+        if choice in ("Y", "y"):
+            _continue = True
+        return _continue
+
+    def set_fy(self, fy=None) -> str | None:
+        currentfy = BftStatus.current.fy()
+        if not fy:
+            msg = f"You did not specify a fiscal year. Current fiscal year [{currentfy}] will be used."
+            self.stdout.write(style_func=self.style.WARNING, msg=msg)
+            if self._get_user_input(currentfy):
+                fy = currentfy
+        else:
+            if fy != currentfy:
+                msg = f"You specified a FY different than current value.\nCurrent is {currentfy}.\nYou provided {fy}"
+                self.stdout.write(style_func=self.style.WARNING, msg=msg)
+                if not self._get_user_input(fy):
+                    fy = None
+
+        return fy
+
+    def set_period(self, period=None) -> str | None:
+        currentperiod = BftStatus.current.period()
+        if not period:
+            msg = f"You did not specify a period. Current period [{currentperiod}] will be used."
+            self.stdout.write(style_func=self.style.WARNING, msg=msg)
+            if self._get_user_input(currentperiod):
+                period = currentperiod
+        else:
+            if period != currentperiod:
+                msg = f"You specified a period different than current value.\nCurrent is {currentperiod}.\nYou provided {period}"
+                self.stdout.write(style_func=self.style.WARNING, msg=msg)
+                if not self._get_user_input(period):
+                    period = None
+        return period
--- bft/management/commands/monthlyencumbrance.py
+++ bft/management/commands/monthlyencumbrance.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 from bft.conf import PERIODKEYS
 from bft.models import BftStatus, CostCenter, CostCenterManager
 from reports.utils import CostCenterMonthlyEncumbranceReport
+from bft.management.commands._private import UserInput
 
 
 class Command(BaseCommand):
@@ -68,48 +69,9 @@ class Command(BaseCommand):
             else:
                 raise ValueError(f"Period [{period}] not valid.  Must be one of {PERIODKEYS}")
 
-    def _get_user_input(self, hint: str) -> bool:
-        choice = input(f"Continue using {hint} ? [y/n] ")
-        if not choice or choice in ("N", "n"):
-            _continue = False
-        if choice in ("Y", "y"):
-            _continue = True
-        return _continue
-
-    def set_fy(self, fy=None) -> str | None:
-        currentfy = BftStatus.current.fy()
-        if not fy:
-            msg = f"You did not specify a fiscal year. Current fiscal year [{currentfy}] will be used."
-            self.stdout.write(style_func=self.style.WARNING, msg=msg)
-            if self._get_user_input(currentfy):
-                fy = currentfy
-        else:
-            if fy != currentfy:
-                msg = f"You specified a FY different than current value.\nCurrent is {currentfy}.\nYou provided {fy}"
-                self.stdout.write(style_func=self.style.WARNING, msg=msg)
-                if not self._get_user_input(fy):
-                    fy = None
-
-        return fy
-
-    def set_period(self, period=None) -> str | None:
-        currentperiod = BftStatus.current.period()
-        if not period:
-            msg = f"You did not specify a period. Current period [{currentperiod}] will be used."
-            self.stdout.write(style_func=self.style.WARNING, msg=msg)
-            if self._get_user_input(currentperiod):
-                period = currentperiod
-        else:
-            if period != currentperiod:
-                msg = f"You specified a period different than current value.\nCurrent is {currentperiod}.\nYou provided {period}"
-                self.stdout.write(style_func=self.style.WARNING, msg=msg)
-                if not self._get_user_input(period):
-                    period = None
-        return period
-
     def run_update(self, fy=None, period=None):
-        fy = self.set_fy(fy)
-        period = self.set_period(period)
+        fy = UserInput().set_fy(fy=fy)
+        period = UserInput().set_period(period=period)
         if not fy or not period:
             self.stdout.write(style_func=self.style.SUCCESS, msg="Operation cancelled")
             return
