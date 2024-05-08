2b096fcc0dfa46b635045f14f4ca088eecbabfb2
Mario St-Gelais <mario.stg@videotron.ca>
2024-05-08 11:02:48 -0400

    implement costcenter monthly allocation report view

--- reports/views.py
+++ reports/views.py
@@ -145,6 +145,41 @@ def allocation_status_report(request):
     return render(request, "allocation-status-report.html", context)
 
 
+def costcenter_monthly_allocation(request):
+    costcenter = fund = fy = ""
+    period = 1
+    context = {}
+    form_filter = True
+    if len(request.GET):
+        costcenter = CostCenterManager().get_request(request)
+        fy = int(request.GET.get("fy")) if request.GET.get("fy") else 0
+        fund = FundManager().get_request(request)
+        period = int(request.GET.get("period")) if request.GET.get("period") else 1
+
+        if str(period) not in PERIODKEYS:
+            messages.warning(request, "Period is invalid.  Either value is missing or outside range")
+
+    initial = {
+        "costcenter": costcenter,
+        "fund": fund,
+        "fy": fy,
+        "period": period,
+    }
+    form = SearchCostCenterMonthlyDataForm(initial=initial)
+    r = utils.CostCenterMonthlyAllocationReport(fy=fy, fund=fund, costcenter=costcenter, period=period)
+    df = r.dataframe()
+    df = df.style.format(thousands=",")
+
+    context = {
+        "table": df.to_html(),
+        "form_filter": form_filter,
+        "form": form,
+        "title": "Cost Center Monthly Allocation",
+        "action": "costcenter-monthly-allocation",
+    }
+    return render(request, "costcenter-monthly-allocation.html", context)
+
+
 def costcenter_monthly_data(request):
     costcenter = fund = fy = ""
     period = 1
