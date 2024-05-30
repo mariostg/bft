bb2bfbd00e7afa5cc2abd89d6ec14a6a958c5393
Mario St-Gelais <mario.stg@videotron.ca>
2024-05-30 14:18:03 -0400

    test for costcenter monthly allocation report on allocation update

--- reports/tests/test_costcentermonthlyallocationreport.py
+++ reports/tests/test_costcentermonthlyallocationreport.py
@@ -49,3 +49,25 @@ class TestCostCenterMonthlyAllocationReport:
 
         cc_alloc = CostCenterMonthlyAllocation.objects.filter(costcenter=self.cc_str).aggregate(Sum("allocation"))
         assert 1010 == float(cc_alloc["allocation__sum"])
+
+    def test_costcenter_monthly_allocation_on_update_allocation(self, populate):
+        """After populate, 8484WA C113 has allocation of 10.  Let's update to 2000."""
+        update_alloc = CostCenterAllocation.objects.get(
+            costcenter=self.costcenter,
+            fund=self.fund,
+            quarter=self.quarter,
+            fy=self.fy,
+        )
+        update_alloc.amount = 2000
+        update_alloc.save()
+
+        CCMAR = CostCenterMonthlyAllocationReport(fy=self.fy, period=self.period, quarter=self.quarter)
+
+        grouped_sum = CCMAR.sum_allocation_cost_center()
+        assert 2000 == float(grouped_sum[0]["allocation"])
+
+        affected_count = CCMAR.insert_grouped_allocation(grouped_sum)
+        assert 2 == affected_count
+
+        cc_alloc = CostCenterMonthlyAllocation.objects.filter(costcenter=self.cc_str).aggregate(Sum("allocation"))
+        assert 2000 == float(cc_alloc["allocation__sum"])
