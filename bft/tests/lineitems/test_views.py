c611bad4e1602de4a3e81d47f54c37815ca180a6
Mario St-Gelais <mario.stg@videotron.ca>
2024-07-30 09:59:48 -0400

    use populatedata and convert to pytest lineitems/test_views.py

--- bft/tests/lineitems/test_views.py
+++ bft/tests/lineitems/test_views.py
@@ -1,18 +1,14 @@
 import pytest
 from django.test import Client
 
-import bft.management.commands.populate as populate
 from bft.models import CostCenter
 
 
-class CostCenterLineItemTest:
-    @pytest.fixture
-    def populate(self):
-        hnd = populate.Command()
-        hnd.handle()
+@pytest.mark.django_db
+class TestCostCenterLineItem:
 
-    def test_url_is_good(self, populate):
+    def test_url_is_good(self, populatedata):
         c = Client()
         cc_id = CostCenter.objects.cost_center("8484wa").id
         response = c.get(f"/bft/lineitem/?costcenter={cc_id}")
-        self.assertEqual(200, response.status_code)
+        assert 200 == response.status_code
