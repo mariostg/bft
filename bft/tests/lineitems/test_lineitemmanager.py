6a76b8278a4e2d8ffa31014e43a48c9efab064e9
Mario St-Gelais <mario.stg@videotron.ca>
2024-07-30 09:12:37 -0400

    use populatedata fixture in test_lineitemmanager.py

--- bft/tests/lineitems/test_lineitemmanager.py
+++ bft/tests/lineitems/test_lineitemmanager.py
@@ -6,30 +6,25 @@ from bft.models import LineItem, LineItemManager
 
 @pytest.mark.django_db
 class TestLineItemManager:
-    @pytest.fixture
-    def populate(self):
-        hnd = populate.Command()
-        hnd.handle()
-
     # Line Items tests
     def test_line_item_dataframe_no_line_items(self):
         r = LineItem
 
         assert True == r.objects.line_item_dataframe().empty
 
-    def test_line_item_dataframe(self, populate, upload):
+    def test_line_item_dataframe(self, populatedata, upload):
         df = LineItemManager().line_item_dataframe()
         assert 0 < len(df)
         assert "Lineitem_ID" in df.columns
         assert "Costcenter_ID" in df.columns
 
-    def test_line_item_dataframe_single_fund(self, populate, upload):
+    def test_line_item_dataframe_single_fund(self, populatedata, upload):
         df = LineItemManager().line_item_dataframe(fund="c113")
         assert 6 == len(df)
         assert "Lineitem_ID" in df.columns
         assert "Costcenter_ID" in df.columns
 
-    def test_line_item_dataframe_single_doctype(self, populate, upload):
+    def test_line_item_dataframe_single_doctype(self, populatedata, upload):
         df = LineItemManager().line_item_dataframe(doctype="co")
         assert 3 == len(df)
         assert "Lineitem_ID" in df.columns
@@ -39,7 +34,7 @@ class TestLineItemManager:
         r = LineItem
         assert True == r.objects.line_item_detailed_dataframe().empty
 
-    def test_line_item_detailed_dataframe(self, populate, upload):
+    def test_line_item_detailed_dataframe(self, populatedata, upload):
         li_df = LineItemManager().line_item_detailed_dataframe()
         assert "int" == li_df.dtypes.Spent
         assert "int" == li_df.dtypes.Forecast
@@ -107,13 +102,13 @@ class TestLineItemManager:
         for c in columns_found:
             assert c in expected_columns
 
-    def test_line_item_detailed_dataframe_single_doctype(self, populate, upload):
+    def test_line_item_detailed_dataframe_single_doctype(self, populatedata, upload):
         df = LineItemManager().line_item_detailed_dataframe(doctype="co")
         assert 3 == len(df)
         assert "Lineitem_ID" in df.columns
         assert "Costcenter_ID" in df.columns
 
-    def test_line_item_detailed_dataframe_single_fund(self, populate, upload):
+    def test_line_item_detailed_dataframe_single_fund(self, populatedata, upload):
         df = LineItemManager().line_item_detailed_dataframe(fund="C113")
         assert 6 == len(df)
         assert "Lineitem_ID" in df.columns
