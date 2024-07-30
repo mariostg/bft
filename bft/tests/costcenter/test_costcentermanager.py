e8985894f98837e76c80f2f7977faf7e2c820a18
Mario St-Gelais <mario.stg@videotron.ca>
2024-07-30 10:15:41 -0400

    use populatedata fixuture in  costcenter/test_costcentermanager.py

--- bft/tests/costcenter/test_costcentermanager.py
+++ bft/tests/costcenter/test_costcentermanager.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import pytest
 
-from bft.management.commands import populate
 from bft.models import (CostCenter, CostCenterAllocation, CostCenterManager,
                         ForecastAdjustment, Fund, FundCenter, Source)
 
@@ -25,11 +24,6 @@ class TestCostCenterManager:
         }
         CostCenter.objects.create(**cc)
 
-    @pytest.fixture
-    def populate(self):
-        hnd = populate.Command()
-        hnd.handle()
-
     # Cost Center Tests
     def test_a_given_cost_center_exists(self, setup):
         assert True == CostCenterManager().exists("8484ZZ")
@@ -47,12 +41,12 @@ class TestCostCenterManager:
         r = CostCenterManager()
         assert 0 == len(r.cost_center_dataframe(pd.DataFrame))
 
-    def test_cost_center_dataframe_no_data(self, populate):
+    def test_cost_center_dataframe_no_data(self, populatedata):
         r = CostCenterManager()
         with pytest.raises(TypeError):
             r.cost_center_dataframe()
 
-    def test_cost_center_dataframe(self, populate):
+    def test_cost_center_dataframe(self, populatedata):
         CCM = CostCenterManager()
         cc = CCM.get_sibblings("2184A3")
         cc_df = CCM.cost_center_dataframe(cc)
@@ -66,12 +60,12 @@ class TestCostCenterManager:
         r = CostCenterManager()
         assert True == r.allocation_dataframe().empty
 
-    def test_allocation_dataframe(self, populate):
+    def test_allocation_dataframe(self, populatedata):
         df = CostCenterManager().allocation_dataframe(costcenter="8484WA")
         assert 1 == len(df)
         assert "Allocation" in df.columns
 
-    def test_allocation_dataframe_columns(self, populate):
+    def test_allocation_dataframe_columns(self, populatedata):
         fund = Fund.objects.all().first()
         costcenter = CostCenterManager().cost_center("8484WA")
         alloc = CostCenterAllocation(fund=fund, costcenter=costcenter, amount=1000)
@@ -84,7 +78,7 @@ class TestCostCenterManager:
     def test_forecast_adjustment_dataframe_empty(self):
         assert True == CostCenterManager().forecast_adjustment_dataframe().empty
 
-    def test_forecast_adjustment_dataframe(self, populate, upload):
+    def test_forecast_adjustment_dataframe(self, populatedata, upload):
         fund = Fund.objects.all().first()
         costcenter = CostCenterManager().cost_center("8484WA")
         ForecastAdjustment(fund=fund, costcenter=costcenter, amount=1000).save()
@@ -93,7 +87,7 @@ class TestCostCenterManager:
         assert 1 == len(df)
         assert "Forecast Adjustment" in df.columns
 
-    def test_is_updatable_cc_object(self, populate):
+    def test_is_updatable_cc_object(self, populatedata):
         mgr = CostCenterManager()
         cc = mgr.cost_center(costcenter="8484wa")
 
@@ -106,7 +100,7 @@ class TestCostCenterManager:
         updatable = mgr.is_updatable(cc)
         assert False == updatable
 
-    def test_is_updatable_cc_string(self, populate):
+    def test_is_updatable_cc_string(self, populatedata):
         mgr = CostCenterManager()
         cc = mgr.cost_center(costcenter="8484wa")
         cc.isupdatable = True
