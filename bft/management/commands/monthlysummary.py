ee25a98e72bd63f67d435a793da5a335e0eb2dd1
Mario St-Gelais <mario.stg@videotron.ca>
2024-11-28 11:52:35 -0500

    correct help message

--- bft/management/commands/monthlysummary.py
+++ bft/management/commands/monthlysummary.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     """
 
-    help = "Update and show monthly allocation."
+    help = "Update and show monthly summary."
 
     def add_arguments(self, parser):
         parser.add_argument(
